e28a2c1c2f46c3ec976b75a4bb56a57f88493cf3
aeraterta <aeraterta@gmail.com>
2025-01-18 00:55:03 +0800

    Updates in functions and unit-test

--- src/adxl345.c
+++ src/adxl345.c
@@ -21,6 +21,39 @@
 #include "adxl345.h"
 
 /**
+ * @brief Write a register value from the ADXL345 accelerometer.
+ *
+ * @param dev_addr The register address to write the value from.
+ * @param data_buffer Pointer to the buffer containing the data to be written.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+
+int8_t adxl345_write_register_value(uint8_t dev_addr, uint8_t *data_buffer) {
+  return i2c_write_bytes(dev_addr, data_buffer);
+}
+
+/**
+ * @brief Read a register value from the ADXL345 accelerometer.
+ *
+ * @param address The register address to read the value from.
+ * @param val Pointer to a variable to store the read register value.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_read_register_value(uint8_t address, uint8_t *val) {
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, address, val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+  return ADXL345_STATUS_SUCCESS;
+}
+
+/**
  * @brief Initializes and sets up the ADXL345 device.
  *
  * Allocates and initializes the device structure and configures it with the
@@ -65,4 +98,367 @@ bool adxl345_online(void) {
     return false;
   }
   return true;
+}
+
+/**
+ * @brief Sets the accelerometer power mode.
+ *
+ * Configures the accelerometer's power mode to optimize power consumption
+ * or performance based on the specified mode.
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param mode   Power mode to be configured for the accelerometer.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_power_mode(adxl345_dev *device, adxl345_power_mode mode) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_BW_RATE, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x10;
+  val = val | mode << ADXL345_POWER_MODE_MASK;
+
+  device->power_mode = mode;
+
+  uint8_t data_buffer[] = {ADXL345_REG_BW_RATE, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Sets the accelerometer measurement mode.
+ *
+ * Configures the accelerometer's measurement mode
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param mode   Power mode to be configured for the accelerometer.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_measure_mode(adxl345_dev *device,
+                                adxl345_measure_mode measure) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_POWER_CTL, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x08;
+  val = val | measure << ADXL345_MEASURE_MASK;
+
+  device->measure = measure;
+
+  uint8_t data_buffer[] = {ADXL345_REG_POWER_CTL, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Configures the Output Data Rate (ODR) for the accelerometer.
+ *
+ * Sets the output data rate of the accelerometer to control how frequently
+ * the device outputs data.
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param mode   Power mode to be configured for the accelerometer.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_odr(adxl345_dev *device, adxl345_odr odr) {
+  uint8_t val = 0x00;
+
+  if (device->power_mode == ADXL345_LOW_POWER_MODE) {
+    if (odr > ADXL345_ODR_400HZ) {
+      // Refer to Table 8
+      return ADXL345_STATUS_INPUT_ERR;
+    }
+  }
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_BW_RATE, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x0F;
+  val = val | odr << ADXL345_ODR_MASK;
+
+  device->odr = odr;
+
+  uint8_t data_buffer[] = {ADXL345_REG_BW_RATE, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Configures the accelerometer scale for the ADXL345 device.
+ *
+ * Sets the full-scale range of the accelerometer based on the
+ * specified scale parameter.
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param mode   Power mode to be configured for the accelerometer.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_scale(adxl345_dev *device, adxl345_scale scale) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_DATA_FORMAT, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x03;
+  val = val | scale << ADXL345_SCALE_MASK;
+
+  device->scale = scale;
+
+  uint8_t data_buffer[] = {ADXL345_REG_DATA_FORMAT, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Configures the accelerometer resolution for the ADXL345 device.
+ *
+ * Sets the resolution of the accelerometer based on the specified
+ * resolution parameter.
+ *
+ * @param device Pointer to the LSM303 device structure.
+ * @param resolution  Resolution setting for the accelerometer.
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_acc_set_resolution(adxl345_dev *device,
+                                  adxl345_resolution resolution) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_DATA_FORMAT, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x08;
+  val = val | resolution << ADXL345_RESOLUTION_MASK;
+
+  device->resolution = resolution;
+
+  uint8_t data_buffer[] = {ADXL345_REG_DATA_FORMAT, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Set the tap x-axis offset for the ADXL345 accelerometer. Offset
+ * scale factor is 15.6mg/LSB
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param offset Offset to be added in the x-axis (-128 to 127)
+ *      -128: -128 x 0.0156 g/LSB = -2g
+ *         0:    0 x 0.0156 g/LSB =  0g
+ *       127:  127 x 0.0156 g/LSB =  2g
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_offset_x(adxl345_dev *device, int8_t offset) {
+  device->offset_config.x = offset;
+
+  uint8_t data_buffer[] = {ADXL345_REG_OFFSET_X, offset};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Set the tap y-axis offset for the ADXL345 accelerometer. Offset
+ * scale factor is 15.6mg/LSB
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param offset Offset to be added in the y-axis (-128 to 127)
+ *      -128: -128 x 0.0156 g/LSB = -2g
+ *         0:    0 x 0.0156 g/LSB =  0g
+ *       127:  127 x 0.0156 g/LSB =  2g
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_offset_y(adxl345_dev *device, int8_t offset) {
+  device->offset_config.y = offset;
+
+  uint8_t data_buffer[] = {ADXL345_REG_OFFSET_Y, offset};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Set the tap z-axis offset for the ADXL345 accelerometer. Offset
+ * scale factor is 15.6mg/LSB
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param offset Offset to be added in the z-axis (-128 to 127)
+ *      -128: -128 x 0.0156 g/LSB = -2g
+ *         0:    0 x 0.0156 g/LSB =  0g
+ *       127:  127 x 0.0156 g/LSB =  2g
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_offset_z(adxl345_dev *device, int8_t offset) {
+  device->offset_config.z = offset;
+
+  uint8_t data_buffer[] = {ADXL345_REG_OFFSET_Z, offset};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+/**
+ * @brief Set the tap threshold for the ADXL345 accelerometer.
+ *
+ *  Sets the tap threshold for both single and double tap. Tap scale factor
+ *  is 62.5mg/LSB
+ *
+ * @param device Pointer to the ADXL345 device structure.
+ * @param threshold The desired tap threshold (0-255)
+ *
+ * @return
+ *   - 0 on success.
+ *   - Non-zero error code on failure.
+ */
+int8_t adxl345_set_tap_threshold(adxl345_dev *device, uint8_t threshold) {
+  device->tap_config.threshold = threshold;
+
+  uint8_t data_buffer[] = {ADXL345_REG_THRESH_TAP, threshold};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// Add doxygen
+int8_t adxl345_set_tap_duration(adxl345_dev *device, int8_t duration) {
+  device->tap_config.duration = duration;
+
+  uint8_t data_buffer[] = {ADXL345_REG_DUR, duration};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_tap_latency(adxl345_dev *device, int8_t latency) {
+  device->tap_config.latency = latency;
+
+  uint8_t data_buffer[] = {ADXL345_REG_LATENT, latency};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_tap_window(adxl345_dev *device, int8_t window) {
+  device->tap_config.window = window;
+
+  uint8_t data_buffer[] = {ADXL345_REG_WINDOW, window};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_activity_threshold(adxl345_dev *device, int8_t threshold) {
+  device->activity_config.activity_threshold = threshold;
+
+  uint8_t data_buffer[] = {ADXL345_REG_THRESH_ACT, threshold};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_inactivity_threshold(adxl345_dev *device, int8_t threshold) {
+  device->activity_config.inactivity_threshold = threshold;
+
+  uint8_t data_buffer[] = {ADXL345_REG_THRESH_INACT, threshold};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_enable_axes_activity(adxl345_dev *device,
+                                    adxl345_axes_enable activity_en) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_ACT_INACT_CTL, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x70;
+  val = val | activity_en << ADXL345_ACTIVITY_EN_MASK;
+
+  device->activity_config.activity_en = activity_en;
+
+  uint8_t data_buffer[] = {ADXL345_REG_ACT_INACT_CTL, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_enable_axes_inactivity(adxl345_dev *device,
+                                      adxl345_axes_enable inactivity_en) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_ACT_INACT_CTL, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x07;
+  val = val | inactivity_en << ADXL345_INACTIVITY_EN_MASK;
+
+  device->activity_config.inactivity_en = inactivity_en;
+
+  uint8_t data_buffer[] = {ADXL345_REG_ACT_INACT_CTL, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_freefall_threshold(adxl345_dev *device, int8_t threshold) {
+  device->freefall_config.threshold = threshold;
+
+  uint8_t data_buffer[] = {ADXL345_REG_THRESH_FF, threshold};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_set_freefall_timeout(adxl345_dev *device, int8_t timeout) {
+  device->freefall_config.timeout = timeout;
+
+  uint8_t data_buffer[] = {ADXL345_REG_TIME_FF, timeout};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+// ADD DOXYGEN
+int8_t adxl345_tap_axes_enable(adxl345_dev *device,
+                               adxl345_axes_enable tap_en) {
+  uint8_t val = 0x00;
+
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_TAP_AXES, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+
+  val &= ~0x07;
+  val = val | tap_en << ADXL345_TAP_EN_MASK;
+
+  device->tap_config.tap_en = tap_en;
+
+  uint8_t data_buffer[] = {ADXL345_REG_TAP_AXES, val};
+  return i2c_write_bytes(ADXL345_I2C_ADDRESS, data_buffer);
+}
+
+int8_t adxl345_get_trigger_source(adxl345_dev *device) {
+  uint8_t val = 0x00;
+  if (i2c_read_byte(ADXL345_I2C_ADDRESS, ADXL345_REG_ACT_TAP_STATUS, &val) !=
+      ADXL345_STATUS_SUCCESS) {
+    return ADXL345_STATUS_API_ERR;
+  }
+  return ADXL345_STATUS_SUCCESS;
 }
--- src/adxl345.h
+++ src/adxl345.h
@@ -41,22 +41,48 @@ typedef enum {
 #define ADXL345_DEV_ID      0xE5
 
 /*********************************MASKS****************************************/
+
+#define ADXL345_POWER_MODE_MASK    0x04
+#define ADXL345_ODR_MASK           0x00
+#define ADXL345_SCALE_MASK         0x01
+#define ADXL345_RESOLUTION_MASK    0x03
+#define ADXL345_MEASURE_MASK       0x03
+#define ADXL345_ACTIVITY_EN_MASK   0x04
+#define ADXL345_INACTIVITY_EN_MASK 0x00
+#define ADXL345_TAP_EN_MASK        0x00
+
 /*********************************REGISTERS************************************/
 
-#define ADXL345_REG_DEV_ID      0x00
-#define ADXL345_REG_BW_RATE     0x2C
-#define ADXL345_REG_POWER_CTL   0x2D
-#define ADXL345_REG_INT_ENABLE  0x2E
-#define ADXL345_REG_INT_MAP     0x2F
-#define ADXL345_REG_INT_SOURCE  0x30
-#define ADXL345_REG_DATAX0      0x32
-#define ADXL345_REG_DATAX1      0x33
-#define ADXL345_REG_DATAY0      0x34
-#define ADXL345_REG_DATAY1      0x35
-#define ADXL345_REG_DATAZ0      0x36
-#define ADXL345_REG_DATAZ1      0x37
-#define ADXL345_REG_FIFO_CTL    0x38
-#define ADXL345_REG_FIFO_STATUS 0x39
+#define ADXL345_REG_DEV_ID         0x00
+#define ADXL345_REG_THRESH_TAP     0x1D
+#define ADXL345_REG_OFFSET_X       0x1E
+#define ADXL345_REG_OFFSET_Y       0x1F
+#define ADXL345_REG_OFFSET_Z       0x20
+#define ADXL345_REG_DUR            0x21
+#define ADXL345_REG_LATENT         0x22
+#define ADXL345_REG_WINDOW         0x23
+#define ADXL345_REG_THRESH_ACT     0x24
+#define ADXL345_REG_THRESH_INACT   0x25
+#define ADXL345_REG_TIME_INACT     0x26
+#define ADXL345_REG_ACT_INACT_CTL  0x27
+#define ADXL345_REG_THRESH_FF      0x28
+#define ADXL345_REG_TIME_FF        0x29
+#define ADXL345_REG_TAP_AXES       0x2A
+#define ADXL345_REG_ACT_TAP_STATUS 0x2B
+#define ADXL345_REG_BW_RATE        0x2C
+#define ADXL345_REG_POWER_CTL      0x2D
+#define ADXL345_REG_INT_ENABLE     0x2E
+#define ADXL345_REG_INT_MAP        0x2F
+#define ADXL345_REG_INT_SOURCE     0x30
+#define ADXL345_REG_DATA_FORMAT    0x31
+#define ADXL345_REG_DATAX0         0x32
+#define ADXL345_REG_DATAX1         0x33
+#define ADXL345_REG_DATAY0         0x34
+#define ADXL345_REG_DATAY1         0x35
+#define ADXL345_REG_DATAZ0         0x36
+#define ADXL345_REG_DATAZ1         0x37
+#define ADXL345_REG_FIFO_CTL       0x38
+#define ADXL345_REG_FIFO_STATUS    0x39
 
 /*****************************ACC DESCRIPTORS**********************************/
 typedef enum {
@@ -65,22 +91,27 @@ typedef enum {
 } adxl345_power_mode;
 
 typedef enum {
+  ADXL345_STANDBY_MODE = 0x00,
+  ADXL345_MEASURE_MODE = 0x01
+} adxl345_measure_mode;
+
+typedef enum {
   ADXL345_ODR_0_10HZ = 0x00,
-  ADXL345_ODR_0_20HZ = 0x00,
-  ADXL345_ODR_0_39HZ = 0x00,
-  ADXL345_ODR_0_78HZ = 0x00,
-  ADXL345_ODR_1_56HZ = 0x00,
-  ADXL345_ODR_3_13HZ = 0x00,
-  ADXL345_ODR_6_25HZ = 0x00,
-  ADXL345_ODR_12_5HZ = 0x00,
-  ADXL345_ODR_25HZ   = 0x00,
-  ADXL345_ODR_50HZ   = 0x00,
-  ADXL345_ODR_100HZ  = 0x00,
-  ADXL345_ODR_200HZ  = 0x00,
-  ADXL345_ODR_400HZ  = 0x00,
-  ADXL345_ODR_800HZ  = 0x00,
-  ADXL345_ODR_1K6HZ  = 0x00,
-  ADXL345_ODR_3K2HZ  = 0x00,
+  ADXL345_ODR_0_20HZ = 0x01,
+  ADXL345_ODR_0_39HZ = 0x02,
+  ADXL345_ODR_0_78HZ = 0x03,
+  ADXL345_ODR_1_56HZ = 0x04,
+  ADXL345_ODR_3_13HZ = 0x05,
+  ADXL345_ODR_6_25HZ = 0x06,
+  ADXL345_ODR_12_5HZ = 0x07,
+  ADXL345_ODR_25HZ   = 0x08,
+  ADXL345_ODR_50HZ   = 0x09,
+  ADXL345_ODR_100HZ  = 0x0A,
+  ADXL345_ODR_200HZ  = 0x0B,
+  ADXL345_ODR_400HZ  = 0x0C,
+  ADXL345_ODR_800HZ  = 0x0D,
+  ADXL345_ODR_1K6HZ  = 0x0E,
+  ADXL345_ODR_3K2HZ  = 0x0F,
 } adxl345_odr;
 
 typedef enum {
@@ -96,28 +127,130 @@ typedef enum {
   ADXL345_RES_16BIT = 0x01,
 } adxl345_resolution;
 
+typedef enum {
+  ACC_AXES_DISABLE_ALL = 0x00,
+  ACC_AXES_ENABLE_X    = 0x01,
+  ACC_AXES_ENABLE_Y    = 0x02,
+  ACC_AXES_ENABLE_Z    = 0x04,
+  ACC_AXES_ENABLE_XY   = 0x03,
+  ACC_AXES_ENABLE_XZ   = 0x05,
+  ACC_AXES_ENABLE_YZ   = 0x06,
+  ACC_AXES_ENABLE_XYZ  = 0x07
+} adxl345_axes_enable;
+
+typedef struct {
+  bool x;
+  bool y;
+  bool z;
+} axes_status;
+
+typedef struct {
+  int8_t x;
+  int8_t y;
+  int8_t z;
+} offset_config;
+
+typedef struct {
+  uint8_t threshold;
+  uint8_t duration;
+  uint8_t latency;
+  uint8_t window;
+  adxl345_axes_enable tap_en;
+  axes_status axes_status;
+} tap_config;
+
+typedef struct {
+  uint8_t activity_threshold;
+  uint8_t inactivity_threshold;
+  adxl345_axes_enable activity_en;
+  adxl345_axes_enable inactivity_en;
+  axes_status axes_status;
+} activity_config;
+
+typedef struct {
+  uint8_t threshold;
+  uint8_t timeout;
+} freefall_config;
+
 /**********************************HANDLES*************************************/
 
 typedef struct {
   adxl345_power_mode power_mode;
   adxl345_resolution resolution;
+  adxl345_measure_mode measure;
   adxl345_scale scale;
   adxl345_odr odr;
+
+  activity_config activity_config;
+  freefall_config freefall_config;
+  offset_config offset_config;
+  tap_config tap_config;
+
   bool is_Setup;
 } adxl345_dev;
 
 typedef struct {
   adxl345_power_mode power_mode;
   adxl345_resolution resolution;
+  adxl345_measure_mode measure;
   adxl345_scale scale;
   adxl345_odr odr;
+
+  activity_config activity_config;
+  freefall_config freefall_config;
+  offset_config offset_config;
+  tap_config tap_config;
+
   bool is_Setup;
 } adxl345_init_param;
 
 /*******************************PROTOTYPES*************************************/
 
+int8_t adxl345_write_register_value(uint8_t dev_addr, uint8_t *data_buffer);
+
+int8_t adxl345_read_register_value(uint8_t address, uint8_t *val);
+
 int8_t adxl345_setup(adxl345_dev *dev, adxl345_init_param adxl345_params);
 
 bool adxl345_online(void);
 
+int8_t adxl345_set_power_mode(adxl345_dev *device, adxl345_power_mode mode);
+
+int8_t adxl345_set_odr(adxl345_dev *device, adxl345_odr odr);
+
+int8_t adxl345_set_scale(adxl345_dev *device, adxl345_scale scale);
+
+int8_t adxl345_acc_set_resolution(adxl345_dev *device,
+                                  adxl345_resolution resolution);
+
+int8_t adxl345_set_measure_mode(adxl345_dev *device,
+                                adxl345_measure_mode measure);
+
+int8_t adxl345_set_offset_x(adxl345_dev *device, int8_t offset);
+
+int8_t adxl345_set_offset_y(adxl345_dev *device, int8_t offset);
+
+int8_t adxl345_set_offset_z(adxl345_dev *device, int8_t offset);
+
+int8_t adxl345_set_tap_threshold(adxl345_dev *device, uint8_t tap_threshold);
+
+int8_t adxl345_set_tap_duration(adxl345_dev *device, int8_t duration);
+
+int8_t adxl345_set_tap_latency(adxl345_dev *device, int8_t latency);
+
+int8_t adxl345_set_tap_window(adxl345_dev *device, int8_t window);
+
+int8_t adxl345_set_activity_threshold(adxl345_dev *device, int8_t threshold);
+
+int8_t adxl345_enable_axes_activity(adxl345_dev *device,
+                                    adxl345_axes_enable activity_en);
+
+int8_t adxl345_enable_axes_inactivity(adxl345_dev *device,
+                                      adxl345_axes_enable inactivity_en);
+
+int8_t adxl345_set_freefall_threshold(adxl345_dev *device, int8_t threshold);
+
+int8_t adxl345_tap_axes_enable(adxl345_dev *device, adxl345_axes_enable tap_en);
+
+int8_t adxl345_get_trigger_source(adxl345_dev *device);
 #endif
